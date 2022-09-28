4eab234d6d96479ec8d7766be2eecf11609f5b79
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-09-27 21:06:37 -0700

    3/4 completed

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,21 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  const container = document.querySelector('#word-container');
+
+  for (const chars of word) {
+    container.insertAdjacentHTML('beforeend', `<div class="letter-box ${chars}"></div>`);
+  }
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  const letterButtons = document.querySelector('#letter-buttons');
+
+  for (const letter of ALPHABET) {
+    letterButtons.insertAdjacentHTML('beforeend', `<button>${letter} </button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,12 +42,13 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  buttonEl.disabled = true;
 };
 
 // This is a helper function we will use in the future
 // It should return `true` if `letter` is in the word
 // For now, you should test it out to make sure it works
+
 
 const isLetterInWord = (letter) => {
   // Replace this with your code
@@ -56,11 +65,13 @@ const isLetterInWord = (letter) => {
   const word = 'hello';
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+  createDivsForChars(word);
 
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
-
+  generateLetterButtons();
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
+  disableLetterButton(buttonEl);
 })();
+
